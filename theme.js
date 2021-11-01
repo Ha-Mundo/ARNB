8df5e09da4a60badcdfd1d277e7e8ed5b20acd7b
Ha-Mundo <trauzzu@gmail.com>
2021-11-01 20:51:38 +0100

    JS comments added

--- theme.js
+++ theme.js
@@ -1,15 +1,18 @@
+// Define which theme should load next
 const themeMap = {
   dark: "light",
   light: "solar",
   solar: "dark",
 };
 
+// Load the existing theme in local storage
 const theme =
   localStorage.getItem("theme") ||
   ((tmp = Object.keys(themeMap)[0]), localStorage.setItem("theme", tmp), tmp);
 const bodyClass = document.body.classList;
 bodyClass.add(theme);
 
+// Change the theme on a button click
 function toggleTheme() {
   const current = localStorage.getItem("theme");
   const next = themeMap[current];
